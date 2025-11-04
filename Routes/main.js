7cc0c5dadab9defd67da0511d89b732a1e27b687
Andygitsketch <aadje001@campus.goldsmiths.ac.uk>
2025-11-04 11:44:06 +0000

    Lab5 04-11

---
+++ Routes/main.js
@@ -0,0 +1,45 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+// Define our data
+var shopData = {shopName: "Drinks R us"};
+// Define our data
+var shopData = {shopName: "The Thirsty Student", 
+                productCategories:["DrinkA", "Wine", "Soft Drinks", "Hot Drinks"]}
+
+
+// Handle the main routes
+//router.get("/", (req, res) => {
+    //res.render("index.ejs",shopData)
+//}); 
+
+router.get("/about", (req, res) => {
+    res.render("about.ejs")
+}); 
+router.get('/',function(req,res){
+    res.render('index.ejs', shopData)
+ });
+
+router.get("/search", (req, res) => {
+    res.render("search.ejs")
+     });
+
+router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send(req.query);
+ });
+router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+}); 
+ 
+router.post("/registered", (req,res) => { 
+  res.send(req.body) 
+
+
+    // Define our data
+//var shopData = {shopName: "The Thirsty Student", 
+                //productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"]}
+
+}); 
+// Export the router object so index.js can access it
+module.exports = router;
